a7459782504d0a982651e89528afdc5070b67fa9
KorsunPavel <pavel.korsun1@gmail.com>
2017-06-13 09:36:11 +0300

    RautersPart1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,49 @@
-import { HeroDetailComponent } from './hero-detail.component';
-import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from "@angular/forms"
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+
 import { AppComponent } from './app.component';
+import { HeroService } from './hero.service';
+import { HeroesComponent } from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { DashboardComponent } from './dashboard.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, ],
-  declarations: [AppComponent , HeroDetailComponent],
-  bootstrap: [AppComponent]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'detail/:id',
+        component: HeroDetailComponent
+      },
+
+    ])
+  ],
+  declarations: [
+    AppComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent
+  ],
+  bootstrap: [AppComponent],
+  providers: [HeroService],
 })
-export class AppModule { }
+
+export class AppModule {
+
+}
